7b1e40fc74d9f4ff852ae0d9e41e1df5dfab4efd
Simon <s1175667@student.windesheim.nl>
2023-03-27 21:09:30 +0200

    Created localDb connection as backup

--- JurniWebApp/Program.cs
+++ JurniWebApp/Program.cs
@@ -3,15 +3,19 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// UNCOMMENT LINES APPROPRIATE TO DESIRED DATABASE TYPE
+// When using SSH tunneling to connect to a remote database
 var connectionString = builder.Configuration.GetConnectionString("JurniWebAppDb");
 builder.Services.AddDbContext<JurniWebAppDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+// When using a local database
+// var connectionString = builder.Configuration.GetConnectionString("JurniWebAppDb_local");
+// builder.Services.AddDbContext<JurniWebAppDbContext>(options => options.UseSqlite(connectionString));
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
